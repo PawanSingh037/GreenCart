d456d223699fd929e7621e83182bf8e605fa0cd6
pawan singh <myselfpawans37@gmail.com>
2025-07-08 02:02:48 +0530

    some minor changes

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5175', 'https://greencartgrocery-frontend.vercel.app']
+const allowedOrigins = ['http://localhost:5173', 'https://greencartgrocery-frontend.vercel.app']
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
 // Middleware configuration
